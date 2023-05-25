f27529d25ef3de8e5d12c315036da2123aea9ee0
Augusto Moraes <moraezaugusto@gmail.com>
2023-05-25 16:46:13 -0300

    item search funcionando

--- src/Components/InputHome/searchAreaItem/index.tsx
+++ src/Components/InputHome/searchAreaItem/index.tsx
@@ -1,19 +1,30 @@
 import { movie } from "../../../Types/movie";
 import { Item } from "./style";
-import {AiOutlineRight} from "react-icons/ai"
-
+import { AiOutlineRight } from "react-icons/ai"
+import { RootState } from "../../../Redux/store";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setMoviePage } from "../../../Redux/Reducers/movieReducer";
 
 
 type itemSearch = {
-    data:movie
+    data: movie
 }
 
-export const ItemSearch = ({data}:itemSearch) => {
-    return(
-        <Item>
+
+export const ItemSearch = ({ data }: itemSearch) => {
+    const nav = useNavigate();
+    const dispatch = useDispatch()
+    const redirection = () => {
+        dispatch(setMoviePage(data))
+        nav(`/InTheaters/${data.title}`)
+    }
+    return (
+        <Item onClick={redirection}>
             <img src={data.pageBanner} />
             <span>{data.title} - {data.subtitle} <br />  <b>{data.category}</b> </span>
-            <AiOutlineRight/>
+            <AiOutlineRight />
         </Item>
     )
 }
